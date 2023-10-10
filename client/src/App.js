5899f69a18f02085175b0f809f3acd1975dba41d
Kakapo <mastertroll123fig@gmail.com>
2023-10-11 12:48:20 +1300

    moving axios defaults

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,8 @@ class App extends Component {
       selectedTaskId: "0",
       filter: "All Tasks",
     };
+    axios.defaults.baseURL = "https://twitter-clone-6p09.onrender.com/api";
+    axios.defaults.withCredentials = true;
   }
 
   handleFilterChange = (filter) => {
@@ -47,8 +49,6 @@ class App extends Component {
   };
 
   render() {
-    axios.defaults.baseURL = "https://twitter-clone-6p09.onrender.com/api";
-    axios.defaults.withCredentials = true;
     return (
       <Provider store={store}>
         <div className="App">
