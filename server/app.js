dc7801b6540b9be0b088d1740c12e85bf0256701
Kakapo <mastertroll123fig@gmail.com>
2023-10-19 08:59:50 +1300

    trying to fix launching problems

--- server/app.js
+++ server/app.js
@@ -44,10 +44,6 @@ var port = process.env.PORT || 5000;
 
 app.use("/api/tasks", taskroute);
 
-app.get("/", function (req, res) {
-  res.json({ reply: "Route for HOME path." });
-});
-
 app.listen(port, function () {
   connect();
   console.log("Server started on port:" + port);
